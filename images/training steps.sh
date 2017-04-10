1a528d1f6e8d320cf04d172beafe83a98754caa6
pddenhar@gmail.com <pddenhar@gmail.com>
2017-04-10 16:33:10 -0500

    Retrain haar with more samples

--- images/training steps.sh
+++ images/training steps.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-opencv_annotation --annotations=annotations.txt --images=images/ 
+opencv_annotation --annotations=annotations.txt --images=positives/ 
 
-opencv_createsamples -info annotations.txt -bg bg.txt -vec positives.txt -w 36 -h 36  
+opencv_createsamples -info annotations.txt -bg bg.txt -vec positives.txt -w 24 -h 24  
 
-opencv_traincascade -data cascade_dir -vec positives.txt -bg bg.txt -numPos 73 -numNeg 4 -w 36 -h 36 
+opencv_traincascade -data cascade_dir -vec positives.txt -bg bg.txt -numPos 114 -numNeg 4 -w 24 -h 24 
