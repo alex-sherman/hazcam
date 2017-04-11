270d8a58c57b2490ddda8582462d0aa7fcc30991
pddenhar@gmail.com <pddenhar@gmail.com>
2017-04-11 14:44:29 -0500

    Switch vehicle_detection.py to use Haar cascades

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -2,37 +2,22 @@ import cv2
 import numpy as np
 
 EDGE_DILATION = 2
+CASCADE_SRC = 'images/cascade_dir/cascade.xml'
 
 class VehicleDetector(object):
     def __init__(self):
-        self.contours = None
-        self.hierarchy = None
         self.img = None
+        self.car_cascade = cv2.CascadeClassifier(CASCADE_SRC)
 
     def run_step(self, img):
         self.img = img
     
     def draw_frame(self, debug, vis, thrs1, thrs2, vdsize):
         height, width, c = vis.shape
-        imgray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-        #imgray = cv2.medianBlur(imgray,3)
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        cars = self.car_cascade.detectMultiScale(gray, 1.2, 4)
 
-        #thresh = cv2.adaptiveThreshold(imgray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-        thresh = cv2.Canny(imgray, thrs1, thrs2, apertureSize=5)
-        
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, np.array([[1] * vdsize] *vdsize))
-
-        image, self.contours, self.hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        self.contours = [cv2.convexHull(cnt) for cnt in self.contours]
-        #print [cv2.contourArea(contour) for contour in self.contours]
-        #self.contours= [contour for contour in self.contours if cv2.contourArea(contour)>vdsize]
-
-        mask = np.zeros((height, width, 3), np.uint8)
-        mask = cv2.drawContours(mask, self.contours, -1, (0,255,0), 1)
-
-
-        
-        
-        vis = mask
+        for (x,y,w,h) in cars:
+            cv2.rectangle(vis,(x,y),(x+w,y+h),(0,0,255),2) 
         return vis
 
